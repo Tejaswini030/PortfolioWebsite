03a985bfb868114d7beeeb48839950f5c63cd343
Tejaswini Kasinadhuni <tejaswinik@Tejaswinis-Laptop.local>
2024-08-28 14:41:57 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Script for smooth scrolling when clicking on navigation links
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
